712dd5669574e2bb4ee0d61cedccbe6707d23762
Colm Conneely <colm.conneely8@mail.dcu.ie>
2021-03-02 22:41:42 +0000

    Alarm test - worked and not working intcn & A1IE bits set to 1 and A1F cleared to 0

--- Device.cpp
+++ Device.cpp
@@ -19,9 +19,9 @@ using namespace std;
 Device::Device() {
 
 	//irrelevant / just to get rid of squiggly lines
-	this->secs = 5;
-	this->mins = 5;
-	this->hours = 5;
+//	this->secs = 5;
+//	this->mins = 5;
+//	this->hours = 5;
 }
 
 
@@ -102,11 +102,11 @@ int Device::setTime(){
 	char writeBuffer[7];  
 	writeBuffer[0] = 0x00; //initial address (superficial write (slave address) - blank values
 	writeBuffer[1] = 0x00; //seconds reg - is in fact register 0 but due to slave set it is bumped to index 1 - write auto incremented
-	writeBuffer[2] = 0x30; //mins
-	writeBuffer[3] = 0x72; //hour - 7 sets 0 & 3x 1's in highest niblle for 12hr mode and PM
-	writeBuffer[4] = 0x05; //day - Friday
-	writeBuffer[5] = 0x05; //date
-	writeBuffer[6] = 0x05; //month
+	writeBuffer[2] = 0x00; //mins
+	writeBuffer[3] = 0x00; //hour - 7 sets 0 & 3x 1's in highest niblle for 12hr mode and PM
+	writeBuffer[4] = 0x01; //day - Friday
+	writeBuffer[5] = 0x01; //date
+	writeBuffer[6] = 0x01; //month
 
 	//initial write to SET
 	if(write(file, writeBuffer, 7) != 7){
@@ -126,12 +126,15 @@ int Device::setTime(){
 		return 1;
 	}
 
-	cout << "Seconds register [ " <<  buffer[0] << " ] set to :  " << bcdToDec(buffer[0]) <<endl;
-	cout << "Minutes register [ " << buffer[1] << " ] set to :  " << bcdToDec(buffer[1]) <<endl;
-	cout << "Hours register [ " << buffer[2] << " ] set to :  " << bcdToDec(buffer[2]) <<endl;
-	cout << "Day register [ " << buffer[3]  << " ] set to : " << bcdToDec(buffer[3]) << endl;
-	cout << "Date register [ " << buffer[4] << " ] set to : " << bcdToDec(buffer[4]) << endl;
-	cout << "Month register [ " << buffer[5] << " ] set to : " << bcdToDec(buffer[5]) << endl;
+//	cout << "Seconds register [ " <<  buffer[0] << " ] set to :  " << bcdToDec(buffer[0]) <<endl;
+//	cout << "Minutes register [ " << buffer[1] << " ] set to :  " << bcdToDec(buffer[1]) <<endl;
+//	cout << "Hours register [ " << buffer[2] << " ] set to :  " << bcdToDec(buffer[2]) <<endl;
+//	cout << "Day register [ " << buffer[3]  << " ] set to : " << bcdToDec(buffer[3]) << endl;
+//	cout << "Date register [ " << buffer[4] << " ] set to : " << bcdToDec(buffer[4]) << endl;
+//	cout << "Month register [ " << buffer[5] << " ] set to : " << bcdToDec(buffer[5]) << endl;
+
+	cout << "Time set \nWeekday : "<< bcdToDec(buffer[3]) <<"\nDate : "<< bcdToDec(buffer[4]) << "\nMonth : "<< bcdToDec(buffer[5]) << "\n\nTime in Hrs : Mins : Secs \n"<< bcdToDec(buffer[2]) << " : " << bcdToDec(buffer[1]) << " : " << bcdToDec(buffer[0]) << endl;
+
 
 //	getTime();
 
@@ -189,65 +192,66 @@ int Device::getTemp(){
 
 }
 
-//void Device::resetPointer(file, 0x00){
-//
-//	char setPointer[1] = 0x00;
-//	if(write(file, setPointer, 1) != 1){
-//		cout<<"Failed to reset pointer.."<<endl;
-//		return 2;
-//	}
-//
-//}
 
 int Device::setAlarm1(){
 
-	char buffer[19];
-	snprintf(buffer, 19, "/dev/i2c-1");
+        char buffer[19];
+        snprintf(buffer, sizeof(buffer), "/dev/i2c-1");
 
-	int file;
-	if((file = open(buffer, O_RDWR) < 0)){
-		cout<<"Failed to open bus! " << endl;
-		return 1;
+        int file;
+        if((file = open(buffer, O_RDWR)) < 0){
+                cout<<"Failed to open Bus"<<endl;
+                return 1;
+        }
+
+        if (ioctl (file, I2C_SLAVE, 0x68) < 0 ){
+                cout<<"Failed to connect to DS3231.."<<endl;
+                return 1;
 	}
-
-
-	if ( ioctl (file, I2C_SLAVE, 0x68) < 0){
-		cout<<"Failed to connect to sensor in set Alarm1!"<<endl;
-		return 1;
-	}
-
 
 	/*set alarm clock for 12:30:10 with all 0's in bit 7 (A1Mx bits) and 1 in DY/DT bit
 	to trigger alarm when day, hours and minutes match*/
 	char writeBuffer[5];
 	writeBuffer[0] = 0x07; //sets pointer to first alarm register @ 0x07 
-	writeBuffer[1] = 0x10;
-	writeBuffer[2] = 0x32;
-	writeBuffer[3] = 0x72;
-	writeBuffer[4] = 0x45;
+	writeBuffer[1] = 0x10; //A1 secs
+	writeBuffer[2] = 0x00; //A1 mins
+	writeBuffer[3] = 0x00; //A1 hours (
+	writeBuffer[4] = 0x41; //A1 day - 4 (0100) sets DY/DT to 1 making alarm trigger by day)
 
 	if(write(file, writeBuffer, 5) != 5){
 		cout<<"Failed to set alarm1 in write"<<endl;
 		return 6;
 	}
 
+	cout<<"Alarm set!"<<endl;
+
 //	resetPointer(file, 0x00);
-//	char setPtr[1] = 0x00;
-//	if(write(file, setPtr, 1)!=1){
-//		cout<<"Failed to reset pointer"<<endl;
-//		return 2;
-//	}
+	//Reset pointer
+	char setPtr[1] = {0x00};
+	if(write(file, setPtr, 1)!=1){
+		cout<<"Failed to reset pointer"<<endl;
+		return 2;
+	}
 
+	if(read(file, buffer, 19) != 19){
+		cout<<"Failed to read in buffer"<<endl;
+		return 1;
+	}
 
-	char setCtrl[2];
+	cout<<"Alarm set for -  Day " << bcdToDec((buffer[10]>>6)) << " at " << bcdToDec(buffer[9]) << " : " << bcdToDec(buffer[8]) << " : " << bcdToDec(buffer[7]) <<  endl;
+
+	char setCtrl[3];
 	setCtrl[0] = 0x0E;
-	setCtrl[1] = 0x04;  //sets control reg to all zeros except INTCN & A1IE
+	setCtrl[1] = 0x05;  // 00000101 sets control reg to all zeros except INTCN & A1IE
+	setCtrl[2] = 0x88; // 10001000 clears the alarm flag bits to zero (A1F set to 1 on match)
 	if(write(file, setCtrl, 2) != 2){
 		cout<<"Failed to set alarm control register .. "<<endl;
 		return 3;
 	}
 
-	close(file);
+	cout<<"Control set"<<endl;
+
+//	close(file);
 
 
 	return 0;
